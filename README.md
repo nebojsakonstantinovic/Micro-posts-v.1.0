22088bdc68ed7f54d21f6cd42d3b02a3f4417e7a
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2018-05-13 14:01:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Microposts
+
+Simple application using babel
+
+### Version
+1.0
+
+## Usage
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To serve in the browser  - Runs webpack-dev-server
+
+```sh
+$ npm start
+```
+
+### Build
+Compile and build
+
+```sh
+$ npm run build
+```
+
+## More Info
+
+### Author
+
+Nebojsa Konstantinovic
---
+++ src/http.js
@@ -0,0 +1,65 @@
+
+/**
+* EasyHTTP Library
+* Library for making HTTP request
+*
+* @version 3.0.0
+* @author Brad Traversy
+* @license Mit
+*
+**/
+
+class EasyHTTP {
+  // Make an Http GET Request
+  async get (url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  // Make an Http POST Request
+  async post (url, data) {
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    const resData = await response.json();
+    return resData;
+}
+
+// Make an Http PUT Request
+async put (url, data) {
+
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    const resData = await response.json();
+    return resData;
+}
+
+// Make an Http DELETE Request
+async delete (url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+    const resData = await response.json();;
+    return resData;
+}
+
+}
+
+export const http = new EasyHTTP();
